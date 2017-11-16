91c2a83db55751c660d81681fa4f8b95fc594931
Nick Simons <>
2017-11-16 16:01:10 +0100

    Nick
    Dit is de config file, deze wordt bij elke pagina geinclude zodat de standaart functionaliteit op elke pagina het zelfde is

---
+++ admin/config.php
@@ -0,0 +1,26 @@
+<?php
+//session starten
+session_start();
+
+//Alles include bestantden
+require_once 'classes/connection.class.php';
+require_once 'functions.php';
+require_once 'classes/user.class.php';
+
+//General classes
+$user = new User();
+
+//Als er op de uitlog knop word geklikt word deze functie aangeroepen, de sessie word verbroken en de cookie word weggehaalt.
+if(isset($_GET['uitloggen'])) {
+    if(isset($_COOKIE['remember'])) {
+        unset($_COOKIE['remember']);
+        setcookie( "remember", '', strtotime( '-30 days' ) );
+    }
+    session_destroy();
+    die(header('Location: index.php'));
+}
+
+//Check als er een sessie is, wanneer hij dr niet is check hij of er een cookie bestaat met de juiste hash.
+if(!$user->isLoggedIn()) {
+    die(header("Location: index.php"));
+}

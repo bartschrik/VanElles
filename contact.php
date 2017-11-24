dbc5c1b2cedb580d1e3f3911342019ac9737f97b
Bart Schrik <s1105662@student.windesheim.nl>
2017-11-24 11:18:56 +0100

    database update
    db update

--- contact.php
+++ contact.php
@@ -3,6 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Contact</title>
+
+    
+    <!--CSS Includes-->
+    <link href="css/lightbox.css" rel="stylesheet" type="text/css"/>
+    <link href="css/slick/slick.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="css/fonts/font-awesome-4.6.3/css/font-awesome.min.css">
+    <link href="http://nl.allfont.net/allfont.css?fonts=franklin-gothic-medium" rel="stylesheet" type="text/css" />
+    <link href="css/main.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 

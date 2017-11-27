d7bcc1674cceb0e801b336b0f5e935fe13ffd52b
Nick Simons <>
2017-11-27 14:52:30 +0100

    Nick
    Homepage design zo goed al af.

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>VanElles</title>
 
+        <!--Google font includes-->
+        <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
+
         <!--CSS Includes-->
         <link href="css/lightbox.css" rel="stylesheet" type="text/css"/>
         <link href="css/slick/slick.min.css" rel="stylesheet" type="text/css"/>
@@ -81,8 +84,8 @@
                     </section>
                     <aside class="col-md-4 col-sm-5 col-xs-12 marbot">
                         <div class="ptitle">
-                            <h1>Uitgelichte</h1>
-                            <h2>Producten</h2>
+                            <h1>Volgende activiteit</h1>
+                            <h2>Workshop</h2>
                         </div>
                         <div class="card">
                             <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
@@ -96,11 +99,143 @@
                 </div>
             </div>
             <div id="main-quote" style="background-image: url('images/stbr2.jpg');">
-
+                <div class="overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-sm-10 col-xs-12 col-md-offset-2 col-sm-offset-1">
+                            <div id="quote-slider">
+                                <div>
+                                    <span class="quote">"Geweldige en vernieuwende winkel, een aanwinst voor het prachtige Rijssen!"</span>
+                                    <hr>
+                                    <span class="naam">Nick Simons</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12" id="main-product">
+                        <div class="a-center">
+                            <div class="ptitle">
+                                <h2>Uitgelichte producten</h2>
+                            </div>
+                        </div>
+                        <div id="product-slide">
+                            <div class="slide">
+                                <div class="card marbot">
+                                    <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                                    <div class="card-body">
+                                        <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                        <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="card marbot">
+                                    <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                                    <div class="card-body">
+                                        <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                        <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="card marbot">
+                                    <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                                    <div class="card-body">
+                                        <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                        <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="card marbot">
+                                    <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                                    <div class="card-body">
+                                        <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                        <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <footer id="main-footer">
-
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 col-xs-12 ft-item">
+                            <div class="ptitle">
+                                <h2 class="dark">Leuk-ER conceptstore</h2>
+                            </div>
+                            <p>Leuk-ER is een conceptstore binnen Van Elles, voor meer informatie <a href="#">klik hier.</a></p>
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-12 ft-item">
+                            <div class="ptitle">
+                                <h2 class="dark">Van Elles Webshop</h2>
+                            </div>
+                            <p>Geen tijd om gezellig in Rijssen te shoppen? Een aantal van onze producten kan u nu online bestellen, <a href="#">klik hier.</a></p>
+                        </div>
+                        <div class="col-md-4 col-sm-12 col-xs-12 ft-item">
+                            <div class="ptitle">
+                                <h2 class="dark">Winkel Locatie</h2>
+                            </div>
+                            <p>Van Elles ligt aan de Rozengaarde 77 in Rijssen, bekijk de locatie <a href="#">hier.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-12 f-item">
+                        <div class="ptitle">
+                            <h2>Openingstijden</h2>
+                        </div>
+                        <table style="width: 100%;">
+                            <tr><td>Maandag: </td><td>9:30 t\m 17:30</td></tr>
+                            <tr><td>Dinsdag: </td><td>9:30 t\m 17:30</td></tr>
+                            <tr><td>Woensdag: </td><td>9:30 t\m 17:30</td></tr>
+                            <tr><td>Donderdag: </td><td>9:30 t\m 21:00</td></tr>
+                            <tr><td>Vrijdag: </td><td>9:30 t\m 17:30</td></tr>
+                            <tr><td>Zaterdag: </td><td>9:30 t\m 17:00</td></tr>
+                            <tr><td>Zondag: </td><td>Gesloten</td></tr>
+                        </table>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12 f-item">
+                        <div class="ptitle">
+                            <h2>Algemene informatie</h2>
+                        </div>
+                        <ul style="list-style: none; padding: 0; margin: 0;">
+                            <li>Van Elles WoonKado&Zo</li>
+                            <li>Rozengaarde 77 7461 DA Rijssen</li>
+                        </ul>
+                        <table style="width: 100%;">
+                            <tr><td>Telefoon: </td><td><a href="tel:0613653118">06 13 65 31 18</a></td></tr>
+                            <tr><td>E-mail: </td><td><a href="mailto:info@vanelles.nl">info@vanelles.nl</a></td></tr>
+                            <tr><td>Kvk: </td><td>0613653118</td></tr>
+                            <tr><td>BTW: </td><td>208147895B01</td></tr>
+                        </table>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12 f-item">
+                        <div class="ptitle">
+                            <h2>Nieuwsbrief</h2>
+                        </div>
+                        <p>Schrijf je nu in voor de nieuwsbrief en blijf op de hoogte van aanbiedingen en nieuws.</p>
+                        <form action="#">
+                            <input type="text" placeholder="E-mailadres">
+                            <input type="submit" value="Aanmelden" class="btn btn-primary">
+                        </form>
+                    </div>
+                </div>
+            </div>
         </footer>
         <!--JS Includes-->
         <script src="js/jquery-3.2.1.js" type="text/javascript"></script>
--- js/main.js
+++ js/main.js
@@ -32,7 +32,6 @@ $(document).ready(function () {
 
     /*Sliders*/
     $('#main-slider').slick({
-        dots: true,
         infinite: true,
         speed: 500,
         fade: true,
@@ -41,6 +40,44 @@ $(document).ready(function () {
         autoplaySpeed: 3000,
         arrows: false,
         dots: false
+    });
+
+    $('#quote-slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: false
+    });
+
+    $('#product-slide').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
 

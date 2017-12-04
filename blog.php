a279e4470e9c570cf4efebcc604f2bc768214822
MichaelMAW97 <33684913+MichaelMAW97@users.noreply.github.com>
2017-12-04 15:20:11 +0100

    Michael
    Blog uploaden werkt. Blog bekijken ook.
    
    Renamen met datum is bezig.

--- blog.php
+++ blog.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HP
- * Date: 15-11-2017
- * Time: 11:01
- */
+    session_start();
+    include_once('includes/header.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog</title>
+</head>
+<body>
+<?php
+    require_once 'admin/classes/connection.class.php';
+    $db = new Connection();
+    $db = $db->databaseConnection();
+
+    $posts = "";
+
+    $dir = 'images/blog/';
+
+    $files = glob("plates/*.{png,jpg,jpeg}", GLOB_BRACE);
+
+    $query1 = $db->prepare('SELECT * FROM blog ORDER BY blog_id DESC');
+
+    $query1->execute();
+
+    while ($row = $query1->fetch(PDO::FETCH_ASSOC)) {
+        $row['blog_id'];
+        $img_name = $row['img_name'];
+        $title = $row['title'];
+        $inhoud = $row['inhoud'];
+
+        echo "<form>
+        <p>$title</p>
+        <p><img height='250' width='250' src='$dir/$img_name'></p>
+        <p>$inhoud</p>
+        </form>";
+    }
+    require_once 'includes/footer.php';
+?>
+</body>
+</html>

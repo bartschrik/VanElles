425708b689a619a5c80df078340ddc2bec30e8a8
TiesPol <tiespolonline@gmail.com>
2017-12-06 09:38:08 +0100

    ties
    quote random

---
+++ admin/classes/content.class.php
@@ -0,0 +1,36 @@
+<?php
+
+class Content {
+
+    private $_db;
+
+    public function __construct()
+    {
+        $this->_db = new Connection();
+        $this->_db = $this->_db->databaseConnection();
+    }
+
+    public function getOneRandomReview() {
+        try {
+            $query = $this->_db->prepare('
+                    SELECT * FROM review JOIN user ON review.user_id=user.user_id ORDER BY RAND() LIMIT 1;
+                ');
+
+            if($query->execute()) {
+                if($query->rowCount() > 0) {
+                    return $content = $query->fetchAll()[0];
+                } else {
+                    return false;
+                }
+            } else {
+                echo $query->errorInfo();
+                return false;
+            }
+        } catch (PDOexception $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+
+
+}

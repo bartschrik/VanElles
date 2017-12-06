edcc6d30ae8ae8b8ee23217236d680efa8635917
TiesPol <tiespolonline@gmail.com>
2017-12-06 12:39:22 +0100

    ties
    datum notatie

--- recensie.php
+++ recensie.php
@@ -25,7 +25,7 @@
 
                 print("<div class='col-xs-12 col-md-6'><div class='card recensiekaart'>");
 
-                print($naamprint . ", " . date("d/m/Y", strtotime($datumprint)) . "<br><br>" . $quoteprint . "<br><br>");
+                print($naamprint . ", " . date("j F Y", strtotime($datumprint)) . "<br><br>" . $quoteprint . "<br><br>");
 
                 for ($i=1; $i <= $ratingprint; $i++){
                     print("<hartjevol class='ion-ios-heart' style='color: #ff00ff; font-size: 30px;'></hartjevol>");

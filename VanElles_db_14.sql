bbab17c7504bf44cd386a37f85991703e7d8915d
Nick Simons <>
2017-12-07 10:54:53 +0100

    Nick
    New db
    page changes

--- VanElles_db_14.sql
+++ VanElles_db_14.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Gegenereerd op: 07 dec 2017 om 10:00
+-- Gegenereerd op: 07 dec 2017 om 10:54
 -- Serverversie: 10.1.26-MariaDB
 -- PHP-versie: 7.1.8
 
@@ -167,7 +167,7 @@ CREATE TABLE `page` (
   `inhoud` mediumtext,
   `description` varchar(2000) DEFAULT NULL,
   `kernwoorden` varchar(100) DEFAULT NULL,
-  `active` int(1) DEFAULT NULL COMMENT '1 = actief, 0 = inactief',
+  `active` int(1) DEFAULT '1' COMMENT '1 = actief, 0 = inactief',
   `datum` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `image` varchar(45) DEFAULT NULL,
   `Module_id` int(11) NOT NULL,
@@ -182,11 +182,9 @@ CREATE TABLE `page` (
 
 INSERT INTO `page` (`id`, `pagetitle`, `title`, `subtitle`, `inhoud`, `description`, `kernwoorden`, `active`, `datum`, `image`, `Module_id`, `user_id`, `url`, `page_order`) VALUES
 (1, 'Home', 'Over de winkel', 'En leuk-ER conceptstore', '<p>Vind jij het ook leuk om naast je webshop een fysiek verkooppunt te hebben, maar is een pand huren voor jou te duur? Ontwerp, maak en cre&euml;er jij de allerleukste dingen, maar heb je net niet dat stukje ondernemerschap in je om het aan de man te brengen?</p>\r\n\r\n<p>Word jij ook vrolijk van dat wat je maakt en wil je eigenlijk wel graag dat het in de winkel ligt?<br />\r\nOf ben jij die leverancier die nog een gaaf verkooppunt zoekt in het Oosten van het land?<br />\r\nDan zijn wij op zoek naar jou! Voor informatie kun je ons een berichtje sturen op hallo@leuk-ER.nl, verzeker je van een plekje in de winkel op een mooie locatie in het centrum van Rijssen en binnenkort zijn jouw spullen te zien en te koop bij&nbsp;Leuk-ER bij Van Elles.</p>\r\n\r\n<p>Tot snel!</p>\r\n\r\n<p>Hartelijke groet,<br />\r\nElles Ligtenberg en Rianne Wessels-Crans</p>\r\n', 'balabl', 'bla, bla, dofs', 1, '2017-12-07 09:52:14', 'image', 1, 1, 'Home', 1),
-(2, 'Blog', 'First inserted page bewerkt', 'een subtitel', '<p>fdsa<strong>fdsaf a&nbsp;<s>&nbsp;fdsaf dsaf</s></strong></p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<h2 style=\"font-style:italic\">fdadfsaf</h2>\r\n', 'Dit is een korte beschrijving van de website voor het seo van google', 'kernwoord1, kernwoord2, panelles', 1, '2017-12-06 21:17:54', NULL, 4, 1, 'Blog', 2),
-(4, 'Contact', 'Contact', 'Stel al uw vragen', '<h1>&nbsp;</h1>\r\n\r\n<p>&nbsp;</p>\r\n', 'fdsafdasfdsa', 'dfasfsaf', 1, '2017-12-07 09:48:17', NULL, 3, 1, 'Contact', 3),
-(10, 'test', 'test', NULL, NULL, NULL, NULL, NULL, '2017-12-06 14:13:40', NULL, 1, 4, 'test', 4),
-(11, 'test1', 'test', NULL, NULL, NULL, NULL, NULL, '2017-12-06 14:58:39', NULL, 1, 4, 'test', 0),
-(12, 'LEUK-ER', 'LEUK-ER', 'Conceptstore', '<p>Al voordat Van Elles in september 2015 haar winkel opende was er een enorme drang om een podium geven aan starters. Mensen die hun eigen mooie producten ontwerpen, maken en online verkopen. Hoe mooi zou het zijn om ook deze groep mensen de mogelijkheid te geven hun producten in een fysieke winkel te verkopen? En Elles weet als geen ander dat de kosten hoog zijn en de risico&rsquo;s groot. Hoe mooi zou het zijn om voor deze mensen wat te betekenen?</p>\r\n\r\n<p>Omdat het runnen van een eigen winkel al de nodige uren met zich meebrengt zocht Elles een samenwerking op en die vond ze.</p>\r\n\r\n<p>Samen met Rianne Wessels-Crans start ze daarom vanaf juni Leuk-ER bij Van Elles.</p>\r\n\r\n<p>Bij Van Elles en bij Leuk-ER valt er altijd wat te beleven. Een nieuwe winkel, een nieuwe ervaring. Het gaat in eerste instantie vooral om beleven, een plek waar mooie spullen en mooie mensen samen komen en er net zo van genieten als wij. Een plek waar je alle tijd hebt om je keuzes te maken, voor jezelf of voor een ander. Laat je verrassen en inspireren. En vooral GENIET!</p>\r\n\r\n<p>Wil jij je&nbsp;<a href=\"#\" target=\"_blank\">aanmelden</a>?</p>\r\n\r\n<p>Nieuwsgierig naar de deelnemers?</p>\r\n', 'LEUK-ER is een conceptstore', 'LEUK-ER, Conceptstore, vanelles, van, Elles', 1, '2017-12-07 09:58:41', NULL, 1, 1, 'LEUK-ER', 0);
+(2, 'Blog', 'First inserted page bewerkt', 'een subtitel', '<p>fdsa<strong>fdsaf a&nbsp;<s>&nbsp;fdsaf dsaf</s></strong></p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<h2 style=\"font-style:italic\">fdadfsaf</h2>\r\n', 'Dit is een korte beschrijving van de website voor het seo van google', 'kernwoord1, kernwoord2, panelles', 0, '2017-12-07 10:06:04', NULL, 4, 1, 'Blog', 2),
+(4, 'Contact', 'Contact', 'Stel al uw vragen', '<h1>&nbsp;</h1>\r\n\r\n<p>&nbsp;</p>\r\n', 'fdsafdasfdsa', 'dfasfsaf', 1, '2017-12-07 10:07:30', NULL, 3, 1, 'Contact', 4),
+(12, 'LEUK-ER', 'LEUK-ER', 'Conceptstore', '<p>Al voordat Van Elles in september 2015 haar winkel opende was er een enorme drang om een podium geven aan starters. Mensen die hun eigen mooie producten ontwerpen, maken en online verkopen. Hoe mooi zou het zijn om ook deze groep mensen de mogelijkheid te geven hun producten in een fysieke winkel te verkopen? En Elles weet als geen ander dat de kosten hoog zijn en de risico&rsquo;s groot. Hoe mooi zou het zijn om voor deze mensen wat te betekenen?</p>\r\n\r\n<p>Omdat het runnen van een eigen winkel al de nodige uren met zich meebrengt zocht Elles een samenwerking op en die vond ze.</p>\r\n\r\n<p>Samen met Rianne Wessels-Crans start ze daarom vanaf juni Leuk-ER bij Van Elles.</p>\r\n\r\n<p>Bij Van Elles en bij Leuk-ER valt er altijd wat te beleven. Een nieuwe winkel, een nieuwe ervaring. Het gaat in eerste instantie vooral om beleven, een plek waar mooie spullen en mooie mensen samen komen en er net zo van genieten als wij. Een plek waar je alle tijd hebt om je keuzes te maken, voor jezelf of voor een ander. Laat je verrassen en inspireren. En vooral GENIET!</p>\r\n\r\n<p>Wil jij je&nbsp;<a href=\"#\" target=\"_blank\">aanmelden</a>?</p>\r\n\r\n<p>Nieuwsgierig naar de deelnemers?</p>\r\n\r\n<p>&nbsp;</p>\r\n', 'LEUK-ER is een conceptstore', 'LEUK-ER, Conceptstore, vanelles, van, Elles', 1, '2017-12-07 10:26:13', NULL, 2, 1, 'LEUK-ER', 3);
 
 -- --------------------------------------------------------
 
@@ -224,8 +222,10 @@ CREATE TABLE `review` (
 --
 
 INSERT INTO `review` (`review_id`, `quote`, `rating`, `user_id`, `datum`, `active`) VALUES
-(1, 'hdajslkfa', 5, 5, '2017-12-06 09:16:54', 0),
-(2, 'Top website!', 5, 7, '2017-12-06 15:32:55', 1);
+(3, 'Super leuke en vernieuwende winkel in Rijssen, met goede klantenservice!', 5, 8, '2017-12-07 10:34:56', 1),
+(4, 'Superhippe woonwinkel, je komt ogen te kort!\r\nElke x weer iets origineels!\r\nAanrader voor leuke cadeautjes.', 4, 9, '2017-12-07 10:35:35', 1),
+(5, 'Superhippe woonwinkel, je komt ogen te kort!\r\nElke x weer iets origineels!\r\nAanrader voor leuke cadeautjes.', 3, 10, '2017-12-07 10:37:03', 1),
+(8, 'shit', 1, 11, '2017-12-07 10:38:52', 0);
 
 -- --------------------------------------------------------
 
@@ -276,7 +276,11 @@ INSERT INTO `user` (`user_id`, `email`, `first_name`, `insertion`, `last_name`, 
 (1, 'nick@twesq.com', 'Nick', '', 'Simons', NULL, 655194576, 'Rijssen', 'Entoshof 23', '7562 VV', 1, 0),
 (4, 'test@email2.nl', 'Ties', '', 'Pol', NULL, 699382393, 'Rijssen', 'Entoshof 23', '7462 VV', 1, 0),
 (5, 'nick.simons@live.nl', 'Nick', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 0),
-(7, 'nick@live.nl', 'Nick', NULL, NULL, NULL, 8473209, NULL, NULL, NULL, 2, 0);
+(7, 'nick@live.nl', 'Nick', NULL, NULL, NULL, 8473209, NULL, NULL, NULL, 2, 0),
+(8, 'bart@live.nl', 'Bart', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 0),
+(9, 'erhan@live.nl', 'Erhan', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 0),
+(10, 'michael@gmail.nl', 'Michael', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 0),
+(11, 'Anja@live.nl', 'Anja', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -415,12 +419,12 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT voor een tabel `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT voor een tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- Beperkingen voor geëxporteerde tabellen
 --
--- module/content.php
+++ module/content.php
@@ -1,3 +1,67 @@
-<div id="content">
-    hallo
+<div class="martop">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="ptitle">
+                    <h1><?php echo $pageContent['title']; ?></h1>
+                    <h2><?php echo $pageContent['subtitle']; ?></h2>
+                </div>
+                <?php echo $pageContent['inhoud']; ?>
+            </div>
+        </div>
+    </div>
+    <?php require_once 'includes/quote.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12" id="main-product">
+                <div class="a-center">
+                    <div class="ptitle">
+                        <h2>Uitgelichte producten</h2>
+                    </div>
+                </div>
+                <div id="product-slide">
+                    <div class="slide">
+                        <div class="card marbot">
+                            <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                            <div class="card-body">
+                                <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <div class="card marbot">
+                            <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                            <div class="card-body">
+                                <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <div class="card marbot">
+                            <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                            <div class="card-body">
+                                <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <div class="card marbot">
+                            <a href="#" style="background-image: url('images/stbr.jpg');" class="card-img"></a>
+                            <div class="card-body">
+                                <a href="#"><h4 class="card-title">Nieuw: STBR</h4></a>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae commodi consectetur doloribus ducimus earum et eum hic inventore iusto magnam nam, nobis quas quasi recusandae repellendus saepe, soluta tempora.</p>
+                                <div class="a-right"><a href="#" class="btn btn-primary">Lees meer</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

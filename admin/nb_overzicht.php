81246b87c3bf0afe6e1f5fe1c66629fcaf6ded07
Bart Schrik <s1105662@student.windesheim.nl>
2017-12-07 11:12:12 +0100

    overzicht
    overzicht

---
+++ admin/nb_overzicht.php
@@ -0,0 +1,18 @@
+<div class="content">
+    <?php
+    include_once('includes/header.php');
+
+
+    ?>
+    <div class="pagetitel marbot">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">Nieuwsbrief <span>overzicht</span></div>
+            </div>
+        </div>
+    </div>
+
+<
+</body>
+
+</html>

26ad5ed7ccc0c3fea551953c51b85ed79f3432b3
TiesPol <tiespolonline@gmail.com>
2017-12-08 11:36:00 +0100

    ties
    leverancier update

--- module/leveranciers.php
+++ module/leveranciers.php
@@ -1,4 +1,4 @@
-<?php include_once 'includes/header.php'?>
+<?php include_once 'includes/header.php' ?>
 <div class="container">
     <div class="row">
         <div class="martop marbot">
@@ -22,19 +22,18 @@
                     $inhoud = $row["inhoud"];
                     $logo = $row["logo"];
 
-                    print("<div class='card'>");
+                    print("<div class='col-xs-12 col-md-4 marbot'><div class='card'>");
 
-                    print("<a href=\"#\" style='background-image: url('images/stbr.jpg');' class=\"card-img\"></a>");
+                   /* print("<a href=\"#\" class='card-img' style='background-image: url('/stbr.jpg');' ></a>");*/
+                    print("<img class='card-img' src='./admin/images/leverancier/" . "$logo" . "'>");
 
                     print("<div class=\"card-body\">");
 
-                    print(" <a href=\"#\"><h4 class=\"card-title\">Nieuw: STBR</h4></a>");
+                    print(" <a href=\"#\"><h4 class=\"card-title\">" . $leveranciernaam . "</h4></a>");
 
-                    print(" <p class=\"card-text\"></p>");
+                    print(" <p class=\"card-text\">" . $inhoud . "</p>");
 
-                    print(" <div class=\"a-right\"><a href=\"#\" class=\"btn btn-primary\">Lees meer</a></div>");
-
-                    print("</div></div>");
+                    print("</div></div></div>");
 
                 }
             ?>
@@ -42,4 +41,4 @@
     </div>
 </div>
 
-<?php include_once 'includes/footer.php'?>
+<?php include_once 'includes/footer.php' ?>

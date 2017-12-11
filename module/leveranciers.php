0bb5991c1854f09f14c96b5bc32ab85b3dc2b991
Erhan <erhanhillebrand@gmail.com>
2017-12-11 10:53:47 +0100

    .

--- module/leveranciers.php
+++ module/leveranciers.php
@@ -7,36 +7,36 @@
             </div>
 
             <?php
-                require_once 'admin/classes/connection.class.php';
-                $db = new Connection();
-                $db = $db->databaseConnection();
+            require_once 'admin/classes/connection.class.php';
+            $db = new Connection();
+            $db = $db->databaseConnection();
 
-                $sql = "SELECT * FROM leveranciers";
-                $stmt = $db->prepare($sql);
+            $sql = "SELECT * FROM leveranciers";
+            $stmt = $db->prepare($sql);
 
-                $stmt->execute();
+            $stmt->execute();
 
-                while ($row = $stmt->fetch())
-                {
-                    $leveranciernaam = $row["naam"];
-                    $inhoud = $row["inhoud"];
-                    $logo = $row["logo"];
+            while ($row = $stmt->fetch())
+            {
+                $leveranciernaam = $row["naam"];
+                $inhoud = $row["inhoud"];
+                $logo = $row["logo"];
 
-                    print("<div class='col-xs-12 col-md-4 col-sm-6 marbot'><div class='card'>");
+                print("<div class='col-xs-12 col-md-4 col-sm-6 marbot'><div class='card'>");
 
-                    echo "<a href='#' style='background-image: url(".constant("local_url")."/admin/images/leverancier/".$logo.");' class='card-img'></a>";
+                echo "<a href='#' style='background-image: url(".constant("local_url")."/admin/images/leverancier/".$logo.");' class='card-img'></a>";
 
-                    //print("<img class='card-img' src='./admin/images/leverancier/" . "$logo" . "'>");
+                //print("<img class='card-img' src='./admin/images/leverancier/" . "$logo" . "'>");
 
-                    print("<div class=\"card-body\">");
+                print("<div class=\"card-body\">");
 
-                    print(" <a href=\"#\"><h4 class=\"card-title\">" . $leveranciernaam . "</h4></a>");
+                print(" <a href=\"#\"><h4 class=\"card-title\">" . $leveranciernaam . "</h4></a>");
 
-                    print(" <p class=\"card-text\">" . $inhoud . "</p>");
+                print(" <p class=\"card-text\">" . $inhoud . "</p>");
 
-                    print("</div></div></div>");
+                print("</div></div></div>");
 
-                }
+            }
             ?>
         </div>
     </div>

f2a91fb9c78e3bd0a1dde21fa93fbe83c49f488f
Nick Simons <>
2017-12-20 10:31:05 +0100

    Nick
    New FontAwesome

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -14,7 +14,7 @@ error_reporting(-1);
     <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, width=device-width, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="css/normalizer.css" />
     <script src="https://code.jquery.com/jquery-2.1.4.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
